09e06099177474f0254aa58211292a751b2427b7
AserHub25 <valorylouissaint@gmail.com>
2026-01-15 15:19:21 -0500

    current commit

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -4,10 +4,10 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
       <Route path="/logout" element={<Logout />} />
-      
     </Routes>
   );
 }
