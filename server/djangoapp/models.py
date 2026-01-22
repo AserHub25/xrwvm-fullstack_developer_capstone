0caa58fe883ee1dc3d9b922cedaed1dfea2ab5f9
AserHub25 <valorylouissaint@gmail.com>
2026-01-22 18:12:44 -0500

    current changes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib import admin
-from .models import CarMake, CarModel
 
 # Create your models here.
 
@@ -33,7 +32,5 @@ class CarModel(models.Model):
 
     def __str__(self):
         return self.name
-
-
 admin.site.register(CarMake)
 admin.site.register(CarModel)
